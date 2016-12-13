73b7a9e015525e15f8bb91444ab3b4bcb3e57def
ios <zgy@zgy.local>
2016-12-13 18:26:53 +0800

    no message

--- CircleChart/CircleProgressView.h
+++ CircleChart/CircleProgressView.h
@@ -12,7 +12,6 @@
 
 @interface CircleProgressView : UIView
 
-
 /**
  *  初始化方法, 模仿仪表盘的形式，默认左边水平时为0度， 顺时针方向为正方向，默认就是正方向画线条
  *
@@ -24,34 +23,20 @@
  */
 - (instancetype)initWithFrame:(CGRect)frame startAngle:(double)sAngle endAngle:(double)eAngle;
 
-
-/** 圆的半径 */
-@property (nonatomic, assign, readonly) float radius;
-
-/** 圆心 */
-@property (nonatomic, assign, readonly) CGPoint center;
-
 /** 线条背景颜色 */
 @property (nonatomic, strong) UIColor *backgroundLineColor;
 
 /** 线条颜色 */
 @property (nonatomic, strong) UIColor *lineColor;
 
-/** 背景圆弧开始的角度（角度制） */
-@property (nonatomic, assign, readonly) double startAngle;
+/** 开始的角度（角度制） */
+@property (nonatomic, assign) double startAngle;
 
-/** 背景圆弧结束的角度（角度制） */
-@property (nonatomic, assign, readonly) double endAngle;
+/** 结束的角度（角度制） */
+@property (nonatomic, assign) double endAngle;
 
 /** 百分比 0~1 */
 @property (nonatomic, assign) double percentage;
-
-/** 比例圆弧结束的角度（角度制） */
-@property (nonatomic, assign, readonly) double progressEndAngle;
-
-
-/** 圆形进度条 */
-@property (nonatomic, strong, readonly) CAShapeLayer *progressLayer;
 
 
 
--- CircleChart/ViewController.m
+++ CircleChart/ViewController.m
@@ -7,13 +7,11 @@
 //
 
 #import "ViewController.h"
-#import "HTCircleProgressView.h"
-
-#define COLOR(R, G, B, A)   [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:A]
+#import "CircleProgressView.h"
 
 @interface ViewController ()
 {
-    HTCircleProgressView *_pView;
+    CircleProgressView *_pView;
 }
 @end
 
@@ -21,26 +19,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = COLOR(31, 39, 48, 1.0);
     
-    HTCircleProgressView *progressView = [[HTCircleProgressView alloc]initWithFrame:CGRectMake(0, 0, 202, 180) startAngle:-50.0 endAngle:230.0];
-    progressView.center = self.view.center;
-    
-    
-    progressView.backgroundLineColor = COLOR(119, 118, 119, 1.0);
-    progressView.lineColor = COLOR(253, 201, 79, 1.0);
-    progressView.line1Color = COLOR(235, 93, 19, 1.0);
+    CircleProgressView *progressView = [[CircleProgressView alloc]initWithFrame:CGRectMake(0, 200, 202, 180) startAngle:-45.0 endAngle:225.0];
+    progressView.backgroundLineColor = [UIColor orangeColor];
+    progressView.lineColor = [UIColor redColor];
     
     [self.view addSubview:progressView];
     _pView = progressView;
 
 
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        _pView.percentage = 0.61;
-        
-        _pView.percentage1 = 0.3;
-    });
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    static double percent = 0.25;
+
+    _pView.percentage = percent;
+    
+    
+    percent += 0.25;
+}
 
 @end
